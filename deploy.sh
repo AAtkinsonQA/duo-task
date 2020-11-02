36448b109f78a62badc402b54f235ad642af2b4b
Luke Benson <55387287+LukeBenson@users.noreply.github.com>
2020-11-02 17:31:36 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ssh -i /home/lukewestside14/.ssh/id_rsa lukewestside14@104.198.74.158 << EOF
+ssh -i /var/lib/jenkins/.ssh/id_rsa lukewestside14@104.198.74.158 << EOF
 sudo docker pull lukebenson1/flask-app:1.0
 sudo docker run -d -p 5000:5000 --name flask-app lukebenson1/flask-app:1.0
 git clone https://github.com/LukeBenson/duo-task.git
