4efd4f089f0eeabbd4306acc7d3668449f940f44
Luke Benson <55387287+LukeBenson@users.noreply.github.com>
2020-11-02 17:26:06 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+ssh -i /home/lukewestside14/.ssh/id_rsa lukewestside14@104.198.74.158 << EOF
+sudo docker pull lukebenson1/flask-app:1.0
+sudo docker run -d -p 5000:5000 --name flask-app lukebenson1/flask-app:1.0
+git clone https://github.com/LukeBenson/duo-task.git
+sudo docker run -d -p 80:80 --mount type=bind,source=./duo-task/nginx.conf,target=/etc/nginx/nginx.conf --name nginx nginx:latest
+EOF
