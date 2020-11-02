ee7f18260eeca9db33c95e8e54350faabfb2d79b
Luke Benson <55387287+LukeBenson@users.noreply.github.com>
2020-11-02 17:40:52 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
 ssh -i /var/lib/jenkins/.ssh/id_rsa lukewestside14@104.198.74.158 << EOF
+if ! [ $(docker network ls | grep duo-task-network) ]; then
+  sudo docker network create duo-task-network
+fi
 sudo docker pull lukebenson1/flask-app:1.0
 if [ $(docker ps -a | grep flask-app) ]; then
   sudo docker rm -f flask-app
 fi
-sudo docker run -d -p 5000:5000 --name flask-app lukebenson1/flask-app:1.0
+sudo docker run -d -p 5000:5000 --name flask-app --network duo-task-network lukebenson1/flask-app:1.0
 if [ -d duo-task ]; then
   rm -rf duo-task
 fi
@@ -13,5 +16,5 @@ git clone https://github.com/LukeBenson/duo-task.git
 if [ $(docker ps -a | grep nginx) ]; then
   sudo docker rm -f nginx
 fi
-sudo docker run -d -p 80:80 --mount type=bind,source=/home/lukewestside14/duo-task/nginx.conf,target=/etc/nginx/nginx.conf --name nginx nginx:latest
+sudo docker run -d -p 80:80 --mount type=bind,source=/home/lukewestside14/duo-task/nginx.conf,target=/etc/nginx/nginx.conf --network duo-task-network --name nginx nginx:latest
 EOF
